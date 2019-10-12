2e6f9a713c44d975b61bc0b88532c0621c37b430
shivamSharmaMWP <shivam.sharma@mavenwave.com>
2019-10-12 22:08:59 +0530

    Initial spring project setup

---
+++ api/src/main/java/com/locker/OnlineLockerApplication.java
@@ -0,0 +1,13 @@
+package com.locker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineLockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineLockerApplication.class, args);
+    }
+}
+
